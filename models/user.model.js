1d781c1ff80f9c68fe0ac854fef6d52af9afef54
divy-11 <devvv.sangwan@gmail.com>
2025-06-07 21:57:33 +0530

    Updated Schema.

--- models/user.model.js
+++ models/user.model.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
     name: String,
     email: { type: String, unique: true },
     password: String,
+    avatarUrl: String
 }, { timestamps: true })
 
 const User = mongoose.model('User', userSchema)
