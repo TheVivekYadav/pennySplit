931a50a65b0e7b390ed7018d7b76043a3f7fa718
thevivekyadav <vivekyadavclasher@gmail.com>
2025-06-15 02:05:43 +0000

    error: import error fixed

--- service/auth.service.js
+++ service/auth.service.js
@@ -1,4 +1,4 @@
-import User from "../models/expense.model.js";
+import User from "../models/user.model.js";
 
 export const updateUserRole = async (userId, role) => {
   if (!["admin", "user"].includes(role)) {
