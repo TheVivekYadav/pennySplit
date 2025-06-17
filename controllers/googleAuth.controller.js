f0d27449508289a16e9a09202d960a1e1a2b9285
thevivekyadav <vivekyadavclasher@gmail.com>
2025-06-17 17:01:32 +0000

    removed console.logs

--- controllers/googleAuth.controller.js
+++ controllers/googleAuth.controller.js
@@ -5,7 +5,6 @@ import User from '../models/user.model.js';
 export const googleAuthRedirect = async (req, res) => {
     const redirectUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=${process.env.GOOGLE_REDIRECT_URI}&response_type=code&scope=openid%20email%20profile`;
     res.redirect(redirectUrl);
-    console.log(redirectUrl)
 };
 
 export const googleAuthCallback = async (req, res) => {
