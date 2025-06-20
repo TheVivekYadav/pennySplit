bedc22261a68de82d354fe51d59ea2c8894513a1
divy-11 <devvv.sangwan@gmail.com>
2025-06-20 19:19:17 +0530

    Added Invite Contact using NodeMailler

--- controllers/contact.controller.js
+++ controllers/contact.controller.js
@@ -1,5 +1,15 @@
 import { Contacts } from "../models/contact.model.js";
-import User  from "../models/user.model.js"
+import User from "../models/user.model.js"
+import nodemailer from 'nodemailer';
+const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    },
+});
 
 const addContact = async (req, res) => {
     try {
@@ -47,9 +57,46 @@ const getContactList = async (req, res) => {
 }
 
 const sendInvite = async (req, res) => {
-    try{
-        //will use nodeMail to send Invites. Need email and SMTP password ?
-    }catch(err){
+    try {
+        const { contact } = req.body;
+        const userId = req.user._id;
+        if (!userId) {
+            return res.status(401).json({ message: "Please login first." });
+        }
+        const transporter = nodemailer.createTransport({
+            host: "smtp.ethereal.email",
+            port: 587,
+            secure: false, // true for 465, false for other ports
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS,
+            },
+        });
+        const invitedBy = await User.findById(userId).select("-password");
+        const info = await transporter.sendMail({
+            from: `"PennySplit Team" <${process.env.EMAIL_USER}>`,
+            to: `${contact}`,
+            subject: "Invite to Join PennySplit",
+            text: `Hi there,
+
+${invitedBy.name} has invited you to join PennySplit — a simple way to track shared expenses and settle up with ease.
+
+Get started now: https://pennySplit.com
+
+See you inside,
+The PennySplit Team`,
+            html: `
+  <p>Hi there,</p>
+  <p><strong>${invitedBy.name}</strong> has invited you to join <strong>PennySplit</strong> — a simple way to track shared expenses and settle up with ease.</p>
+  <p><a href="https://pennySplit.in">Click here to join PennySplit</a></p>
+  <p>See you inside,<br/>The PennySplit Team</p>
+`,
+
+        });
+
+        console.log("Message sent:", info.messageId);
+        res.status(200).json({ message: "Invite sent successfully" });
+    } catch (err) {
         res.status(500).json({ message: "backend error", error: err.message });
     }
 }
